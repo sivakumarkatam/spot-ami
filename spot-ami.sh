09d1f554528d8074cf8eb8c1acb3354c12974c96
sivakumarkatam <siva.katam@blazeclan.com>
2017-12-18 15:46:18 +0530

    Update spot-ami.sh

--- spot-ami.sh
+++ spot-ami.sh
@@ -13,14 +13,13 @@ cd /opt/
 sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 sudo tar -xvzf apache-maven-3.3.9-bin.tar.gz
 sudo mv apache-maven-3.3.9 maven
-echo '
-export M2_HOME=/opt/maven
+echo 'export M2_HOME=/opt/maven
 export PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/mavenenv.sh
 cat /etc/profile.d/mavenenv.sh
 sudo chmod +x /etc/profile.d/mavenenv.sh
 cd /etc/profile.d/
 ls
-source /etc/profile.d/mavenenv.sh
+source ./etc/profile.d/mavenenv.sh
 sh ./mavenenv.sh
 mvn --version
 
